012a569d429c7b650b34119341066345bb160ebe
Izzan Abdul <izzanabdul123@gmail.com>
2024-01-09 11:52:22 +0700

    Revert "feat: fixing users page"
    This reverts commit 8592ab2017343a1cf581ec9aa8050e2bbadf2072.

--- src/app/users/page.jsx
+++ src/app/users/page.jsx
@@ -1,33 +1,12 @@
-'use client';
-import AddBar from '@/components/AddBar';
-import SearchBar from '@/components/SearchBar';
 import UsersList from '@/components/UsersList';
-import Pagination from '@/components/utils/Pagination';
-import { useEffect, useState } from 'react';
 import { getDataResponse } from '../libs/api-libs';
 
-const UsersPage = () => {
-  const [usersData, setUsersData] = useState();
-  const [page, setPage] = useState(1);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const data = await getDataResponse('/users', `page=${page}&per_page=10`);
-        setUsersData(data);
-      } catch (err) {
-        console.error(err);
-      }
-    };
-    fetchData();
-  }, [page]);
+const UsersPage = async () => {
+  const data = await getDataResponse('/users', 'page=1&per_page=10');
 
   return (
     <main className="max-w-screen-md mx-auto p-5">
-      <SearchBar />
-      <AddBar />
-      <UsersList data={usersData} />
-      <Pagination page={page} setPage={setPage} />
+      <UsersList data={data} />
     </main>
   );
 };
