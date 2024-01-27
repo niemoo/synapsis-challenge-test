69564dd49bba6ba15a6b5567595692e9c09eca12
Izzan Abdul <izzanabdul123@gmail.com>
2024-01-27 18:56:30 +0700

    feat: update metadata

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -5,8 +5,8 @@ import './globals.css';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
-  title: 'Izzan Test',
-  description: 'Synapsis Front End Developer Challenge Test',
+  title: 'Public Posts from gorest API',
+  description: 'This website using gorest API to handle public posts',
 };
 
 export default function RootLayout({ children }) {
